1f2a319efe228989fd2115b1c081f43da8813333
Pedro Arthur <168201229+Panther511Br@users.noreply.github.com>
2024-05-16 15:23:23 -0300

    Add files via upload

---
+++ Usando if else.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int num
+
+		cout << "Digite um numero";
+	cin >> num
+
+		if (num % 2 == 0 && num % 3 == 0) {
+			cout << "Numero e divisivel por 2 e 3";
+		}
+		else {
+			cout << "O numero nao e divisivel por 2 e 3";
+		}
+}
